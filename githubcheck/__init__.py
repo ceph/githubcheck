c1492da061efa915fc35e4b739cb6611e452fa0c
Kefu Chai <kchai@redhat.com>
2021-05-04 13:27:40 +0800

    add sphinx parser to render errors from sphinx
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- githubcheck/__init__.py
+++ githubcheck/__init__.py
@@ -1,8 +1,10 @@
 from .check import Check
 from .annotation import AnnotationLevel, Annotation
 from .flake8 import Parser as Flake8
+from .sphinx import Parser as Sphinx
 
 __all__ = ['Check',
            'AnnotationLevel',
            'Annotation',
-           'Flake8']
+           'Flake8',
+           'Sphinx']
---
+++ githubcheck/sphinx.py
@@ -0,0 +1,78 @@
+import os.path
+import re
+
+from collections import namedtuple
+
+from .annotation import AnnotationLevel, Annotation
+
+
+class Parser:
+    def __init__(self, base_dir, title):
+        self.title = title
+        # build finished with problems, 2 warnings.
+        # or
+        # succeeded
+        self.succeed_re = r'build succeeded'
+        self.problem_re = r'build finished with problems'
+        base_dir = os.path.abspath(base_dir)
+        # ... /<path>/doc/foo.rst:108: WARNING: something goes wrong!
+        warning_re1 = (f'{base_dir}/'
+                       r'(?P<path>[^:]+):'
+                       r'(?P<lineno>\d+):\s'
+                       r'(?P<level>WARNING|ERROR):\s'
+                       r'(?P<message>.+)$')
+        self.warning_re1 = re.compile(warning_re1)
+        # ... /<path>/doc/foo.rst: WARNING: something goes wrong!
+        warning_re2 = (f'{base_dir}/'
+                       r'(?P<path>[^:]+):\s'
+                       r'(?P<level>WARNING|ERROR):\s'
+                       r'(?P<message>.+)$')
+        self.warning_re2 = re.compile(warning_re2)
+        self.summary = None
+
+    def _parse_level(self, level):
+        if level == 'WARNING':
+            return AnnotationLevel.WARNING
+        elif level == 'ERROR':
+            return AnnotationLevel.FAILURE
+        else:
+            return AnnotationLevel.NOTICE
+
+    def scan(self, output):
+        last_line = None
+        for line in output:
+            matched = self.warning_re1.search(line)
+            if matched:
+                path = matched.group('path')
+                lineno = int(matched.group('lineno'))
+                level = matched.group('level')
+                message = matched.group('message')
+                yield Annotation(path,
+                                 lineno, lineno,
+                                 self._parse_level(level),
+                                 message,
+                                 None, None,
+                                 title=self.title,
+                                 raw_details=line)
+                continue
+            matched = self.warning_re2.search(line)
+            if matched:
+                path = matched.group('path')
+                level = matched.group('level')
+                message = matched.group('message')
+                yield Annotation(path,
+                                 0, 0,
+                                 self._parse_level(level),
+                                 message,
+                                 None, None,
+                                 title=self.title,
+                                 raw_details=line)
+                continue
+            last_line = line
+        if re.match(self.succeed_re, last_line):
+            self.conclusion = 'success'
+        elif re.match(self.problem_re, last_line):
+            self.conclusion = 'failure'
+        else:
+            self.conclusion = 'failure'
+        self.summary = last_line
