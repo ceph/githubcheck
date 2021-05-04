d3aaa06f3fa43124f5a0a2da15852acc9a9b4df6
Kefu Chai <kchai@redhat.com>
2021-05-04 13:27:00 +0800

    annotation: do not set attr in a namedtuple
    raise an exception instead. as NamedTuple attributes are read-only.
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- githubcheck/annotation.py
+++ githubcheck/annotation.py
@@ -25,11 +25,9 @@ class Annotation(NamedTuple):
         assert(len(self.message) <= 64 * 1024)
         assert(self.raw_details is None or len(self.raw_details) <= 64 * 1024)
 
-        if self.end_line is None:
-            self.end_line = self.start_line
         # this is required even though it's not explicitly noted.
-        if self.end_column is None:
-            self.end_column = self.start_column
+        if self.start_column is not None and self.end_column is None:
+            raise ValueError('should specify both start column and end column')
 
         # use the enum's str value in the dict
         d = self._replace(annotation_level=self.annotation_level.value)
